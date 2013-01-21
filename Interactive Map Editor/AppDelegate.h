ba21311433bba00649fc7ad9c53a618a25c24055
Dmitry Davidov <dmitrii.davidov@gmail.com>
2013-01-21 19:22:52 +0400

    Initial Commit

---
+++ Interactive Map Editor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Interactive Map Editor
+//
+//  Created by Dmitry Davidov on 21.01.13.
+//  Copyright (c) 2013 Dmitry Davidov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Interactive Map Editor/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Interactive Map Editor
+//
+//  Created by Dmitry Davidov on 21.01.13.
+//  Copyright (c) 2013 Dmitry Davidov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
