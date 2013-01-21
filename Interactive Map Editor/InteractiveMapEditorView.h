774bfcfe9bf48a3e3732a90b2a3fdfee91af3fc4
Dmitry Davidov <dmitrii.davidov@gmail.com>
2013-01-21 19:26:55 +0400

    Инициация.

---
+++ Interactive Map Editor/InteractiveMapEditorView.h
@@ -0,0 +1,13 @@
+//
+//  InteractiveMapEditorView.h
+//  Interactive Map Editor
+//
+//  Created by Dmitry Davidov on 21.01.13.
+//  Copyright (c) 2013 Dmitry Davidov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface InteractiveMapEditorView : NSView
+
+@end
---
+++ Interactive Map Editor/InteractiveMapEditorView.m
@@ -0,0 +1,27 @@
+//
+//  InteractiveMapEditorView.m
+//  Interactive Map Editor
+//
+//  Created by Dmitry Davidov on 21.01.13.
+//  Copyright (c) 2013 Dmitry Davidov. All rights reserved.
+//
+
+#import "InteractiveMapEditorView.h"
+
+@implementation InteractiveMapEditorView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    
+}
+
+@end
